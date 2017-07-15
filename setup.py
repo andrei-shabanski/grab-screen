6eda83549de0f0cf3baf02c14608902572ff6518
Andrei Shabnaksi <shabanski.andrei@gmail.com>
2017-07-16 01:00:13 +0300

    Configure setup details

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from grab_screen import __version__
 
@@ -7,11 +7,30 @@ with open('requirements/main.txt') as req_file:
 
 setup(
     name='grab-screen',
+    description='Take screenshots and upload anywhere',
     version=__version__,
-    py_modules=['grab_screen'],
+    license='MIT',
+    url='https://github.com/andrei-shabanski/grab-screen',
+    author='Andrei Shabanski',
+    author_email='andrei.shabanski@gmail.com',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
     install_requires=requirements,
-    entry_points="""
-        [console_scripts]
-        grab-screen=grab_screen:main
-    """,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'grab-screen=grab_screen:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Information Technology',
+        'Topic :: Multimedia :: Graphics :: Capture',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='screenshot cloudapp'
 )
